b587649705e6c84a73a03a3bd4c1ad77c9b430bc
marno van Niekerk <marnovn071@gmail.com>
2021-10-21 14:33:46 +0200

    Initial commit

---
+++ src/test/java/com/luminous/fusion/FusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminous.fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
