874a1c72f75f202d77d20bccbb6e7b15ad3936c4
Marno van Niekerk <marnovn071@gmail.com>
2022-01-04 16:45:10 +0200

    Added Dockerfile

--- src/test/java/com/luminous/fusion/FusionApplicationTests.java
+++ src/test/java/com/luminous/fusion/FusionApplicationTests.java
@@ -3,11 +3,12 @@ package com.luminous.fusion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"ENV_PLATFORM = local"})
 class FusionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    void contextLoads() {
+    }
 
 }
