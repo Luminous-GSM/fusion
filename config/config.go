25450741cb2d07c5e206bb820dfb0cfb9a022a7e
Marno van Niekerk <marnovn071@gmail.com>
2022-09-22 10:56:35 +0200

    Changed config json names.

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ var (
 type PodConfiguration struct {
 	TmpfsSize         string   `default:"100" yaml:"tmpfs_size" json:"tmpfsSize"`
 	ContainerPidLimit int64    `default:"512" yaml:"container_pid_limit" json:"containerPidLimit" `
-	Dns               []string `default:"[\"1.1.1.1\", \"1.0.0.1\"]"`
+	Dns               []string `default:"[\"1.1.1.1\", \"1.0.0.1\"]" json:"dns"`
 }
 
 type Configuration struct {
@@ -20,34 +20,34 @@ type Configuration struct {
 
 	// |-----> System CONFIGURATION <-----|
 	// The root directory where fusion data is stored.
-	RootDirectory string `default:"/var/lib/fusion/" validate:"endswith=/" env:"FUSION_SYSTEM_ROOT_DIRECTORY" yaml:"system_root_directory" json:"system_root_directory"`
+	RootDirectory string `default:"/var/lib/fusion/" validate:"endswith=/" env:"FUSION_SYSTEM_ROOT_DIRECTORY" yaml:"system_root_directory" json:"systemRootDirectory"`
 	// Directory where logs and events are logged.
-	LogDirectory string `default:"/var/log/fusion/" validate:"endswith=/" env:"FUSION_SYSTEM_LOG_DIRECTORY" yaml:"system_log_directory" json:"system_log_directory"`
+	LogDirectory string `default:"/var/log/fusion/" validate:"endswith=/" env:"FUSION_SYSTEM_LOG_DIRECTORY" yaml:"system_log_directory" json:"systemLogDirectory"`
 	// Directory where the server data is stored at.
-	DataDirectory  string `default:"/var/lib/fusion/volumes/" validate:"endswith=/" env:"FUSION_SYSTEM_DATA_DIRECTORY" yaml:"system_data_directory" json:"system_data_directory"`
-	CertsDirectory string `default:"/var/lib/fusion/certs/" validate:"endswith=/" env:"FUSION_SYSTEM_CERTS_DIRECTORY" yaml:"system_certs_directory" json:"system_certs_directory"`
+	DataDirectory  string `default:"/var/lib/fusion/volumes/" validate:"endswith=/" env:"FUSION_SYSTEM_DATA_DIRECTORY" yaml:"system_data_directory" json:"systemDataDirectory"`
+	CertsDirectory string `default:"/var/lib/fusion/certs/" validate:"endswith=/" env:"FUSION_SYSTEM_CERTS_DIRECTORY" yaml:"system_certs_directory" json:"systemCertsDirectory"`
 	// User IDs
-	SystemUserUid int `default:"1000" env:"FUSION_SYSTEM_UID" yaml:"system_uid" json:"system_uid"`
-	SystemUserGid int `default:"1000" env:"FUSION_SYSTEM_GID" yaml:"system_gid" json:"system_gid"`
+	SystemUserUid int `default:"1000" env:"FUSION_SYSTEM_UID" yaml:"system_uid" json:"systemUid"`
+	SystemUserGid int `default:"1000" env:"FUSION_SYSTEM_GID" yaml:"system_gid" json:"systemGid"`
 	// |-----> System CONFIGURATION <-----|
 
 	// |-----> NODE CONFIGURATION <-----|
-	NodeUniqueId    string `validate:"required" env:"FUSION_NODE_UNIQUE_ID,required" yaml:"node_unique_id" json:"node_unique_id"`
-	NodeHostname    string `default:"localhost" validate:"alphanum" env:"FUSION_NODE_HOSTNAME" yaml:"node_hostname" json:"node_hostname"`
-	NodeName        string `default:"Fusion" env:"FUSION_NODE_NAME" json:"node_name" yaml:"node_name"`
-	NodeDescription string `default:"Node Control Plane" env:"FUSION_NODE_DESCRIPTION" json:"node_description" yaml:"node_description"`
+	NodeUniqueId    string `validate:"required" env:"FUSION_NODE_UNIQUE_ID,required" yaml:"node_unique_id" json:"nodeUniqueId"`
+	NodeHostname    string `default:"localhost" validate:"alphanum" env:"FUSION_NODE_HOSTNAME" yaml:"node_hostname" json:"nodeHostname"`
+	NodeName        string `default:"Fusion" env:"FUSION_NODE_NAME" json:"nodeName" yaml:"node_name"`
+	NodeDescription string `default:"Node Control Plane" env:"FUSION_NODE_DESCRIPTION" json:"nodeDescription" yaml:"node_description"`
 	// |-----> NODE CONFIGURATION <-----|
 
 	// |-----> API CONFIGURATION <-----|
-	ApiHost          string `default:"0.0.0.0" validate:"url|ip" env:"FUSION_API_HOST" yaml:"api_host" json:"api_host"`
-	ApiPort          int    `default:"8899" validate:"numeric" env:"FUSION_API_PORT" yaml:"api_port" json:"api_port"`
-	ApiSecurityToken string `validate:"required" env:"FUSION_API_SECURITY_TOKEN,required" yaml:"api_token" json:"api_token"`
+	ApiHost          string `default:"0.0.0.0" validate:"url|ip" env:"FUSION_API_HOST" yaml:"api_host" json:"apiHost"`
+	ApiPort          int    `default:"8899" validate:"numeric" env:"FUSION_API_PORT" yaml:"api_port" json:"apiPort"`
+	ApiSecurityToken string `validate:"required" env:"FUSION_API_SECURITY_TOKEN,required" yaml:"api_token" json:"apiToken"`
 	// |-----> API CONFIGURATION <-----|
 
-	ConsoleUrl    string `validate:"required,url|ip" env:"FUSION_CONSOLE_URL,required" json:"console_url" yaml:"console_url"`
-	ManagementUrl string `validate:"required,url|ip" env:"FUSION_MANAGEMENT_URL,required" json:"management_url" yaml:"management_url"`
+	ConsoleUrl    string `validate:"required,url|ip" env:"FUSION_CONSOLE_URL,required" json:"consoleUrl" yaml:"console_url"`
+	ManagementUrl string `validate:"required,url|ip" env:"FUSION_MANAGEMENT_URL,required" json:"managementUrl" yaml:"management_url"`
 
-	AllowPrivateNetwork bool `default:"false" env:"FUSION_ALLOW_PRIVATE_NETWORK" json:"allow_private_network" yaml:"allow_private_network"`
+	AllowPrivateNetwork bool `default:"false" env:"FUSION_ALLOW_PRIVATE_NETWORK" json:"allowPrivateNetwork" yaml:"allow_private_network"`
 
 	// |-----> Pod CONFIGURATION <-----|
 	Pod PodConfiguration `yaml:"pod" json:"pod"`
